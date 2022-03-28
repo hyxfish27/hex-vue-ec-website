98fb409c4b465f2e9e2f7e350087de6a4d1043c6
hyxfish27 <avicky1515@gmail.com>
2022-03-28 11:49:06 +0800

    User Page layout

---
+++ src/views/LayoutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="d-flex flex-column min-vh-100">
+    <FrontNav></FrontNav>
+    <router-view />
+    <FootBar class="mt-auto"></FootBar>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import FrontNav from '@/components/Front/FrontNav.vue'
+import FootBar from '@/components/FootBar.vue'
+
+export default {
+  components: {
+    FrontNav,
+    FootBar
+  }
+}
+</script>

4b7a9427001b6d38b161f49e3e6faefa74ada732
hyxfish27 <avicky1515@gmail.com>
2022-03-28 11:54:18 +0800

    後台驗證&頁面初始

---
+++ src/views/Dashboard/DashboardView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="d-flex flex-column min-vh-100">
+    <DashboardNav></DashboardNav>
+    <div class="container-fluid mt-3 position-relative">
+      <ToastMessage></ToastMessage>
+      <router-view v-if="adminStatus" />
+    </div>
+  </div>
+</template>
+
+<script>
+import DashboardNav from '@/components/Dashboard/DashboardNav.vue'
+import emitter from '@/methods/eventBus'
+import ToastMessage from '@/components/ToastMessage.vue'
+
+export default {
+  data () {
+    return {
+      adminStatus: false
+    }
+  },
+  components: {
+    DashboardNav,
+    ToastMessage
+  },
+  provide () {
+    return {
+      emitter
+    }
+  },
+  methods: {
+    checkAdmin () {
+      const token = document.cookie.replace(
+        /(?:(?:^|.*;\s*)myToken\s*=\s*([^;]*).*$)|^.*$/,
+        '$1'
+      )
+      // token 存在，存取登入權限
+      if (token) {
+        this.$http.defaults.headers.common.Authorization = `${token}`
+        const url = `${process.env.VUE_APP_API}/api/user/check`
+        this.$http
+          .post(url)
+          .then(() => {
+            this.adminStatus = true
+          })
+          .catch(err => {
+            alert(err.data.message)
+            this.$router.replace('/login')
+          })
+      } else {
+        alert('請重新登入 > <')
+        this.$router.push('/login')
+      }
+    }
+    // 登出寫在 Navbar
+  },
+  mounted () {
+    this.checkAdmin()
+  }
+}
+</script>

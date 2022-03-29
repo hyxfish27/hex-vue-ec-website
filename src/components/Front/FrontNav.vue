8d4200744936001bbe18824951c1f6938f436d18
hyxfish27 <avicky1515@gmail.com>
2022-03-29 09:55:17 +0800

    FrontNav 美化

--- src/components/Front/FrontNav.vue
+++ src/components/Front/FrontNav.vue
@@ -1,10 +1,10 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-white sticky-top" id="frontNav">
+  <nav class="navbar navbar-expand-lg bg-white sticky-top justify-content-center" id="frontNav">
     <div class="container">
       <router-link class="navbar-brand" to="/">
-        <div class="text-align-justify">
-          女 子 x 生 活<br />
-          Good Life
+        <div id="brandName">
+          <i class="bi bi-flower1 h3"></i>
+          <span class="h3">女 子 x 生 活 Good Life</span>
         </div>
       </router-link>
       <button
@@ -16,24 +16,38 @@
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+      <i class="bi bi-justify navbar-toggler-icon h2 text-primary my-auto"></i>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
+        <div class="navbar-nav text-center">
           <router-link class="nav-link" to="/products">
-            逛逛商品
+            <i class="bi bi-handbag-fill d-none d-lg-inline-flex h4"></i>
+            <p class="mb-0 h6">我要逛逛</p>
           </router-link>
           <router-link class="nav-link" to="/about">
-            關於女子生活
+            <i class="bi bi-chat-left-dots-fill d-none d-lg-inline-flex h4"></i>
+            <p class="mb-0 h6">我想認識</p>
           </router-link>
         </div>
-        <div class="navbar-nav ms-auto">
-          <router-link class="nav-link" to="/favorite"> 收藏 </router-link>
+        <div class="navbar-nav ms-auto text-center">
           <router-link class="nav-link" to="/cart">
-            購物車
+            <i class="bi bi-bag-fill d-none d-lg-inline-flex h4"></i>
+            <p class="mb-0 h6">我要下單</p>
+          </router-link>
+          <router-link class="nav-link" to="/favorite">
+            <i class="bi bi-heart-fill d-none d-lg-inline-flex h4"></i>
+            <p class="mb-0 h6">我收藏的</p>
           </router-link>
         </div>
       </div>
     </div>
   </nav>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Kaisei+Decol:wght@500&display=swap');
+
+#brandName {
+  font-family: 'Kaisei Decol', serif;
+}
+</style>

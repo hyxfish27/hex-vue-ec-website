7885827f9f075500c2e8caad77ad315f0c5ac951
hyxfish27 <avicky1515@gmail.com>
2022-03-31 20:18:38 +0800

    FavoriteView init

---
+++ src/views/FavoriteView.vue
@@ -0,0 +1,30 @@
+<template>
+  <HeaderPic></HeaderPic>
+  <h2>this is favorite page</h2>
+  <div v-for="(id, key) in favorites" :key="id + key">
+    <p>{{ id }}</p>
+  </div>
+</template>
+
+<script>
+import HeaderPic from '@/components/Front/HeaderPic.vue'
+
+export default {
+  data () {
+    return {
+      favorites: []
+    }
+  },
+  components: {
+    HeaderPic
+  },
+  methods: {
+    getFavorites () {
+      this.favorites = JSON.parse(localStorage.getItem('favorites'))
+    }
+  },
+  mounted () {
+    this.getFavorites()
+  }
+}
+</script>

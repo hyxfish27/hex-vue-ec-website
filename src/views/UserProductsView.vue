064388d1e6ed6ca14b30e46c2a0aa8a8bc35d4d4
hyxfish27 <avicky1515@gmail.com>
2022-03-31 20:17:24 +0800

    收藏功能初始

--- src/views/UserProductsView.vue
+++ src/views/UserProductsView.vue
@@ -1,79 +1,181 @@
 <template>
   <HeaderPic></HeaderPic>
-  <div class="container">
-    <div class="row mb-2 mb-md-4 justify-content-around">
-      <button class="btn btn-primary text-white col-5 col-md-2 rounded-pill">
-        分類 1
+  <Loading :is-ready="isReady"></Loading>
+  <div class="container bg-secondary rounded-3 mb-0 mb-md-4">
+    <div class="row justify-content-around py-4">
+      <button
+        class="btn btn-primary text-white col-5 col-md-2 rounded-pill mb-3 mb-md-0"
+        @click="getCategory(小資家電)"
+      >
+        小資家電
       </button>
-      <button class="btn btn-primary text-white col-5 col-md-2 rounded-pill">
-        分類 2
+      <button
+        class="btn btn-primary text-white col-5 col-md-2 rounded-pill mb-3 mb-md-0"
+        @click="getCategory(質感居家)"
+      >
+        質感居家
       </button>
-      <button class="btn btn-primary text-white col-5 col-md-2 rounded-pill">
-        分類 3
+      <button
+        class="btn btn-primary text-white col-5 col-md-2 rounded-pill mb-md-0"
+        @click="getCategory(精緻生活)"
+      >
+        精緻生活
       </button>
-      <button class="btn btn-primary text-white col-5 col-md-2 rounded-pill">
-        分類 4
+      <button
+        class="btn btn-primary text-white col-5 col-md-2 rounded-pill mb-md-0"
+        @click="getCategory(療癒小物)"
+      >
+        療癒小物
       </button>
     </div>
+
     <div class="row">
       <div
         class="col-12 col-md-6 col-lg-4"
         v-for="(product, key) in products"
         :key="product.id + key"
+        style="cursor: pointer;"
+        @click="$router.push(`/product/${product.id}`)"
       >
-        <div class="card mb-4 rounded-3">
+        <div class="card mb-4 rounded-3 border-primary border-1 shadow-info">
           <div
             :style="{ backgroundImage: `url(${product.imageUrl})` }"
-            style="height: 300px; background-size: cover; background-position: center center"
-          ></div>
+            style="height: 15rem; background-size: cover; background-position: center center"
+            class="p-3"
+          >
+            <span
+              class="text-white bg-success rounded-3 p-1"
+              v-if="product.origin_price !== product.price"
+              >特價中</span
+            >
+          </div>
           <div class="card-body">
             <h5 class="card-title">
-              <router-link :to="`/product/${product.id}`">
+              <router-link class="link-primary" :to="`/product/${product.id}`">
                 {{ product.title }}
               </router-link>
             </h5>
-            <div class="row">
-              <div class="col-lg-6">
-                <p class="card-text text-primary fs-4">
-                  NT$ {{ product.price }}
-                </p>
+
+            <div class="row justify-content-center align-items-center">
+              <div class="col-12 text-primary h4 mb-3">
+                <div class="price">NT ${{ product.price }}</div>
               </div>
-              <div class="col-lg-6"></div>
-              <div class="col-lg-6"></div>
+              <div class="col-6 mb-3" v-if="!toggleFavorBtn(product.id)">
+                <button
+                  class="btn btn-primary w-100
+                  text-center text-white text-nowrap"
+                  @click.stop="toggleFavorites(product.id)"
+                >
+                  <i class="bi bi-heart-fill h6"></i>
+                  <span class="h6">加入收藏</span>
+                </button>
+              </div>
+              <div class="col-6 mb-3" v-else>
+                <button
+                  class="btn btn-success w-100
+                  text-center text-white text-nowrap"
+                  @click.stop="toggleFavorites(product.id)"
+                >
+                  <i class="bi bi-heart-fill h6"></i>
+                  <span class="h6">移除收藏</span>
+                </button>
+              </div>
+              <div class="col-6 mb-3">
+                <button
+                  class="btn btn-primary w-100
+                  text-center text-white text-nowrap"
+                  @click.stop="innerButton"
+                >
+                  <i class="bi bi-bag-fill h6"></i>
+                  <span class="h6">加入購物車</span>
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <!-- Pagination -->
+    <Pagination :pages="pagination" @emit-pages="getProducts"></Pagination>
   </div>
   <router-view />
 </template>
 
 <script>
 import HeaderPic from '@/components/Front/HeaderPic.vue'
-// import ProductModal from '@/components/ProductModal.vue'
+import Pagination from '@/components/Pagination.vue'
+import Loading from '@/components/Loading.vue'
+
 export default {
   data () {
     return {
       products: [],
       productId: '',
-      isLoading: ''
+      isLoading: '',
+      isReady: '',
+      pagination: {},
+      favorites: []
     }
   },
   components: {
-    HeaderPic
-    // ProductModal
+    HeaderPic,
+    Pagination,
+    Loading
   },
+  inject: ['emitter'],
   methods: {
-    getProducts () {
-      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products/all`
+    getFavorites () {
+      this.favorites = JSON.parse(localStorage.getItem('favorites'))
+      if (this.favorites == null) {
+        this.favorites = []
+      }
+    },
+    toggleFavorites (id) {
+      this.getFavorites()
+      const index = this.favorites.indexOf(id)
+      if (index === -1) {
+        this.favorites.push(id)
+        this.emitter.emit('push-message', {
+          style: 'success',
+          title: '收藏成功',
+          emoji: 'emoji-heart-eyes'
+        })
+      } else {
+        this.favorites.splice(index, 1)
+        this.emitter.emit('push-message', {
+          style: 'dark',
+          title: '移除收藏',
+          emoji: 'emoji-dizzy'
+        })
+      }
+      localStorage.setItem('favorites', JSON.stringify(this.favorites))
+    },
+    toggleFavorBtn (id) {
+      const liked = this.favorites.includes(id)
+      return liked
+    },
+    innerButton () {
+      alert('這是內層')
+    },
+    test (text) {
+      alert(text)
+    },
+    getProducts (page = 1) {
+      this.isReady = false
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products?page=${page}`
       this.$http
         .get(url)
         .then(res => {
           this.products = res.data.products
+          this.pagination = res.data.pagination
+          this.isReady = true
         })
         .catch(err => {
-          alert(err.data.message)
+          this.emitter.emit('push-message', {
+            style: 'danger',
+            title: '取得產品失敗',
+            content: err.response.data.message
+          })
         })
     },
     addToCart (id, qty = 1) {
@@ -103,6 +205,7 @@ export default {
   },
   mounted () {
     this.getProducts()
+    this.getFavorites()
   }
 }
 </script>

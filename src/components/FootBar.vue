7940c9597227f5e9fd128b62beb9d24837a461cd
hyxfish27 <avicky1515@gmail.com>
2022-03-31 20:14:01 +0800

    FootBar 版面整理

--- src/components/FootBar.vue
+++ src/components/FootBar.vue
@@ -1,22 +1,30 @@
 <template>
-  <footer class="bg-secondary py-4">
+  <footer class="bg-dark py-4">
     <div class="container">
-      <div class="row justify-content-center align-items-center">
-        <div class="col-12 col-md-4">
-          <p class="mb-2 text-center text-white">聯絡資訊</p>
+      <div class="row justify-content-center align-items-center mb-4">
+        <div class="col-12 col-md-6 text-secondary">
+          <p class="mb-2 h5">聯絡我們</p>
+          <i class="bi bi-envelope h5"></i>
+          <span>girlxgoodlife@gmail.com</span>
         </div>
-        <div class="col-12 col-md-4">
-          <p class="mb-2 text-center text-white">追蹤我們</p>
-          <p>facebook</p>
-          <p>instagram</p>
-          <p>line</p>
+        <div class="col-12 col-md-6 text-secondary">
+          <p class="mb-2 h5">追蹤女子生活</p>
+          <i class="bi bi-facebook h5"></i>
+          <i class="bi bi-instagram h5"></i>
+          <i class="bi bi-github h5"></i>
         </div>
-        <div class="col-12 col-md-4">
-          <p class="mb-2 text-center text-white">Copyright</p>
-          <router-link class="btn btn-primary text-white m-auto" to="/login">
-            管理員登入
-          </router-link>
-        </div>
+      </div>
+      <div class="text-center">
+        <span class="mb-2 text-secondary h6 mr-1 mr-sm-3">
+          &copy;&nbsp;Vicky Huang | 僅個人作品練習，無任何商業用途
+        </span>
+        <router-link
+          class="text-secondary m-auto p-1"
+          to="/login"
+        >
+          <i class="bi bi-gear mr-1 mr-sm-3"></i>
+          <span>管理員登入</span>
+        </router-link>
       </div>
     </div>
   </footer>

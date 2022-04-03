3b37efecb81435a2a702dacd312ef63753f5d03a
hyxfish27 <avicky1515@gmail.com>
2022-04-03 21:09:03 +0800

    AboutView init

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,18 @@
 <template>
   <HeaderPic :title="title"></HeaderPic>
-  <h2>This is about page</h2>
+  <div class="container-fluid px-0 text-center">
+    <div class="row">
+      <div class="col-md-7">
+        <img src="@/assets/about.jpg" alt="girls" class="w-100" />
+      </div>
+      <div class="col-md-5">
+        <h2>我們三個是大學認識的好朋友，出社會了之後，發現一個人住有好多</h2>
+      </div>
+    </div>
+    <router-link to="/" class="btn btn-primary text-white h4 my-3"
+      >回首頁</router-link
+    >
+  </div>
 </template>
 
 <script>

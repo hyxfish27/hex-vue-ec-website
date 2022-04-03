9e3b3cb1bd8fe2cbada95c344f518889e82d67bd
hyxfish27 <avicky1515@gmail.com>
2022-04-03 19:48:55 +0800

    AboutView init

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,16 @@
 <template>
-  <HeaderPic></HeaderPic>
+  <HeaderPic :title="title"></HeaderPic>
   <h2>This is about page</h2>
 </template>
 
 <script>
 import HeaderPic from '@/components/Front/HeaderPic.vue'
 export default {
+  data () {
+    return {
+      title: '關於女子'
+    }
+  },
   components: {
     HeaderPic
   }
